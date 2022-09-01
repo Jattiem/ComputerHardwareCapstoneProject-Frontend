d189fd3df8b21e83b0523c6ec84e8ab5c050fea6
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-09-01 09:11:53 +0200

    Frontend

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -66,7 +66,7 @@
   transform: rotate(-20deg);
   }
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
